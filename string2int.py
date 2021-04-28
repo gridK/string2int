13ddf23ed65c643777b59b843364ad73fa212bae
Grid <sanjaikornsutatipkul@gmail.com>
2021-04-28 18:45:00 +0700

    OutPutChanged

--- string2int.py
+++ string2int.py
@@ -1,6 +1,12 @@
 import re
 input = input()
-out = re.findall(r"\d+", input)
 # compare an input to the regular expression format of decimal
-for i in out:
-    print(i, end="")
+integers = ["0","1","2","3","4","5","6","7","8","9"]
+output = 0
+for i in input:
+    for j in integers:
+        if i == j :
+            output = ((output * 10) + integers.index(j)) 
+
+print(output)
+
